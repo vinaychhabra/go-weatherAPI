50b07d288cacdf5f15e88b12d20733423424e28c
vinaychhabra <vchhabra10feb@gmail.com>
2024-03-26 16:15:06 -0400

    external api response type added

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+// ExternalAPIResponse defines the structure for the external weather API response
+type ExternalAPIResponse struct {
+	Temperature string `json:"temperature"`
+	Wind        string `json:"wind"`
+	Description string `json:"description"`
+	Forecast    []struct {
+		Day         string `json:"day"`
+		Temperature string `json:"temperature"`
+		Wind        string `json:"wind"`
+	} `json:"forecast"`
+}
+
 // WeatherResponse defines the structure of our API response
 type WeatherResponse struct {
 	City        string `json:"city"`
